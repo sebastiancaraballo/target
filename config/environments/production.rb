1a49400eb6b61d78983056f9673c7bc223e6f828
Sebastián Caraballo <sebastiancaruy1@gmail.com>
2018-12-07 12:51:56 -0300

    Fix AWS credentials issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   routes.default_url_options = { host: ENV['HOST'] }
+  config.require_master_key = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
